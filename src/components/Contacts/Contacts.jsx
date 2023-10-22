6980d095c22cd91cf0335be945f5da63daf13fcc
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-10-22 22:16:25 +0200

    Contacts Style css

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import s from "./Contacts.module.css"
 
 const Contacts = ({ contacts,onContactDelete }) => {
   return (
 
-    <ul>
+    <ul className={s.list}>
       {contacts.map((contact) => (
-        <li key={contact.id}>{contact.name} - {contact.number}
-         <button onClick={() => onContactDelete(contact.id)}>Usuń</button>
+        <li className={s.contact} key={contact.id}>{contact.name} - {contact.number}
+         <button className={s.btn} onClick={() => onContactDelete(contact.id)}>Delete</button>
          </li>
       ))}
     </ul>
