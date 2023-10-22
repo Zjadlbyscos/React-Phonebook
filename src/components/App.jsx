81c2831aab0006cdd19b6ed0ee19ee1bbc90fe1f
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-10-22 20:51:08 +0200

    Module ContactForm create, relocate from App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Contacts from './Contacts/Contacts';
-// import nanoid from 'nanoid';
 import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js';
+
+import Contacts from './Contacts/Contacts';
+import ContactForm from './ContactForm/ContactForm';
 
 class App extends Component {
   constructor(props) {
@@ -13,32 +14,20 @@ class App extends Component {
       number: '',
     };
   }
-  handleSubmit = e => {
-    e.preventDefault();
-
-    // if (!this.state.name.match(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)) {
-    //   return;
-    // }
-    // if (!this.state.number.match(/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/)) {
-    //   return;
-    // }
-
-    // const id = nanoid();
-
+  handleContactSubmit = ({ name, number }) => {
     const contact = {
       id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
+      name,
+      number,
     };
 
     this.setState({
       contacts: [...this.state.contacts, contact],
-      name: '',
-      number: '',
+  
     });
   };
 
-  handleFilterChange = (e) => {
+  handleFilterChange = e => {
     this.setState({
       filter: e.target.value.toLowerCase(),
     });
@@ -48,27 +37,7 @@ class App extends Component {
     return (
       <div>
         <h1>Książka telefoniczna</h1>
-        <form onSubmit={this.handleSubmit}>
-        <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={e => this.setState({ name: e.target.value })}
-            required
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          />
-          <input
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={e => this.setState({ number: e.target.value })}
-            required
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          />
-          <button type="submit">Dodaj kontakt</button>
-        </form>
+        <ContactForm onSubmit={this.handleContactSubmit} />
         <input
           type="text"
           placeholder="Wyszukaj"
@@ -80,11 +49,11 @@ class App extends Component {
     );
   }
 
-getFilteredContacts() {
-  return this.state.contacts.filter((contact) => {
-    return contact.name.toLowerCase().includes(this.state.filter);
-  });
-}
+  getFilteredContacts() {
+    return this.state.contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(this.state.filter);
+    });
+  }
 }
 
 export default App;
