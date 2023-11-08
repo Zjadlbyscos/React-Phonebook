52d3e31224f39a01ea89c07006938f244359db3c
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-08 13:58:13 +0100

    const App.jsx instead of class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
-import useAddContact from 'hooks/useAddContact.js';
+import { nanoid } from 'nanoid';
 
 import Contacts from './Contacts/Contacts';
 import ContactForm from './ContactForm/ContactForm';
@@ -9,7 +8,7 @@ import Filter from './Filter/Filter';
 import style from './App.module.css';
 
 const App = () => {
-  const [contacts, handleContactSubmit] = useAddContact([
+  const [contacts, setContacts] = useState([
     { id: 'id-1', name: 'Anthony Kiedis', number: '459-12-56' },
     { id: 'id-2', name: 'Chad Smith', number: '645-17-79' },
     { id: 'id-3', name: 'Damiano David', number: '645-17-79' },
@@ -22,18 +21,56 @@ const App = () => {
     // Load contacts from localStorage if they exist
     const savedContacts = localStorage.getItem('contacts');
     if (savedContacts) {
-      handleContactSubmit(JSON.parse(savedContacts));
+      setContacts(JSON.parse(savedContacts));
     }
-  }, [handleContactSubmit]);
+  }, []);
 
   useEffect(() => {
     //component dudUpdate
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
- 
+  const handleContactSubmit = ({ name, number }) => {
+    const isDuplicate = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
 
-  
+    if (isDuplicate) {
+      alert('A contact with this name already exists!');
+      return;
+    }
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts([...contacts, contact]);
+  };
+
+  const handleContactDelete = contactId => {
+    const shouldDelete = window.confirm(
+      'Are you sure you want to delete this contact?'
+    );
+
+    if (shouldDelete) {
+      const updatedContacts = contacts.filter(
+        contact => contact.id !== contactId
+      );
+
+      setContacts(updatedContacts);
+    }
+  };
+
+  const handleFilterChange = e => {
+    setFilter(e.target.value.toLowerCase());
+  };
+
+  const getFilteredContacts = () => {
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter);
+    });
+  };
 
   return (
     <div className={style.form}>
